b4e5a84f25f6221072d77de8fc1a860e68b90a63
daviddickson <tianchengdavid@gmail.com>
2026-09-14 01:36:52 +1200

    added file for future voltage clamp experiment script

---
+++ DynamicalNeuroscience/voltage_clamp_experiment.py
@@ -0,0 +1,8 @@
+# voltage_clamp_experiment.py
+'''
+variate applied current to hold voltage a constant 'clamp voltage'
+'''
+
+# first restructure the differential equation to set dV as whatever the error to the desired voltage clamp is
+# ion currents are calculated at present voltage, and applied current makes up the difference.
+
